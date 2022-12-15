dc5cda398dd1bacdc39f4445dd05a454b5dc639c
Edgard <20133252@ealoe.ulima.edu.pe>
2022-12-15 09:12:32 -0500

    Initial Commit

---
+++ collapsibleHeaderRefresh/collapsibleHeaderRefreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  collapsibleHeaderRefreshApp.swift
+//  collapsibleHeaderRefresh
+//
+//  Created by EdgardVS on 15/12/22.
+//
+
+import SwiftUI
+
+@main
+struct collapsibleHeaderRefreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
